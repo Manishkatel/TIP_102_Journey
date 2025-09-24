2b8e116fb47d219042197e677a890dc0b19cd2d7
Manish Katel <167320626+Manishkatel@users.noreply.github.com>
2025-09-24 10:24:40 -0500

    reversing a linked list with iterative way

---
+++ reverse_linked_list.py
@@ -0,0 +1,32 @@
+"Reversing the linked list python"
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def printList(node):
+    while node is not None:
+        print(f"{node.data}", end="")
+        if node.next is not None:
+            print(" -> ", end="")
+        node = node.next
+    print()
+
+def is_inversed(head):
+    prev = None
+    current = head
+    while current:
+        nxt = current.next      # save next
+        current.next = prev     # reverse link
+        prev = current          # move prev forward
+        current = nxt           # move current forward
+    return prev   # new head of reversed list
+        
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(4)
+head.next.next.next.next = Node(5)
+head = is_inversed(head)
+printList(head)
